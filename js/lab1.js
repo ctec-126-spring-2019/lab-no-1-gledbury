c885ee973384e86c0cb8c2bf43c122dd5b4e55d5
gledbury <gledbury4@gmail.com>
2019-04-17 19:02:52 -0700

    George Ledbury

--- js/lab1.js
+++ js/lab1.js
@@ -1,12 +1,12 @@
 // lab1.js
-
+// George Ledbury
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
-    let output = "<h2>Bruce's Skills<h2>";
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];// added the missing bracket
+    let output = "<h2>Bruce's Skills</h2>";// added the missing / to the closing h2 tag
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
         output += '<li>' + skills[i] + '</li>';
@@ -16,14 +16,14 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+    let firstName = 'Bruce'; // added the missing = and the missing ;
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    let collegeName = 'Clark College'; // added the missing ;
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;// added the missing comma and $'s so string template will work
 
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';// changed the " to '
     document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
